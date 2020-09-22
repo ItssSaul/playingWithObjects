ffab5ff299536848d01da8f29747a0ab1945f40b
ItssSaul <70920638+ItssSaul@users.noreply.github.com>
2020-09-22 18:07:10 -0500

    Add files via upload

---
+++ playingWithObjects/objects.js
@@ -0,0 +1,20 @@
+var users = [
+        {
+            name: "Michael",
+            age: 37
+        },
+        {
+            name: "John",
+            age: 30
+        },
+        {
+            name: "David",
+            age: 27
+        }
+    ];
+    console.log(users[2].age);
+    console.log(users[1].name);
+
+    for(var i = 0; i <users.length;i++){
+        console.log(users[i]);
+    }
